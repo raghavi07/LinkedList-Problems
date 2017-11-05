6a99579b08ad4ca2c0ad2689df8708d599090415
Your Name <you@example.com>
2017-11-05 14:39:23 +0530

    PR addressed

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,7 @@
 package linkedListFunctions;
 
+import linkedListFunctions.LinkedList.Node;
+
 public class LinkedList {
 	Node head;
 
@@ -12,77 +14,12 @@ public class LinkedList {
 			next = null;
 		}
 	}
-
-	public void insertMiddle(int data, int toInsert) {
-		Node newNode = new Node(toInsert);
-		if (this.head == null) {
-			System.out.println("  Head is null ");
-			return;
-		}
-		Node temp = this.head;
-		while (temp != null) {
-			if (temp.data == data) {
-				System.out.println("  Found after node to be inserted ");
-				newNode.next = temp.next;
-				temp.next = newNode;
-			}
-			temp = temp.next;
-		}
-		System.out.println("** Not Found after node to be inserted **");
-	}
-
-	public void insertMiddleUsingPosition(int pos, int data) {
-		Node newNode = new Node(data);
-		if (this.head == null) {
-			System.out.println("  Head is null ");
-			if (pos == 1) {
-				this.head = newNode;
-			}
-			return;
-		}
-		Node temp = this.head;
-		Node prev = this.head;
-		int count = 1;
-		while (temp != null) {
-			if (count == pos) {
-				System.out.println("  Found after node to be inserted ");
-				prev.next = newNode;
-				newNode.next = temp;
-			}
-			count++;
-			prev = temp;
-			temp = temp.next;
-		}
-		System.out.println("** Not Found after node to be inserted **");
-	}
-
-	public void insertEnd(int data) {
-		Node newNode = new Node(data);
-		if (this.head == null) {
-			System.out.println("  Head is null ");
-			this.head = newNode;
-			return;
-		}
-		Node temp = this.head;
-		while (temp.next != null) {
-			temp = temp.next;
-		}
-		System.out.println("** node inserted at the last **");
-		temp.next = newNode;
-	}
-
-	public void insertStart(int data) {
-		Node newNode = new Node(data);
-		newNode.next = this.head;
-		this.head = newNode;
-	}
-
+	
 	public void print() {
-		if(head==null){
-			System.out.println("Linked List is Empty !!!");
-			return ;
+		if (head == null) {
+			return;
 		}
-		System.out.println(" ----- Linked List ----- ");
+
 		Node temp = this.head;
 		while (temp != null) {
 			System.out.print(temp.data + "   ");
@@ -90,73 +27,7 @@ public class LinkedList {
 		}
 	}
 
-	public void deleteMiddleData(int data) {
-		if (this.head == null) {
-			System.out.println("  Head is null ");
-		}
-		Node temp = this.head;
-		Node prev = this.head;
-		while (temp != null) {
-			if (temp.data == data) {
-				System.out.println("  Found data to be deleted ");
-				prev.next = temp.next;
-			}
-			prev = temp;
-			temp = temp.next;
-
-		}
-		System.out.println("** Not Found delete data **");
-	}
-
-	public void deleteMiddleUsingPosition(int pos) {// change it to
-		// position
-		if (this.head == null) {
-			System.out.println("  Head is null ");
-			return;
-		}
-		Node temp = this.head;
-		Node prev = this.head;
-		int count = 0;
-		while (temp != null) {
-			count++;
-			if (count == pos) {
-				System.out.println("  Found data to be deleted ");
-				prev.next = temp.next;
-			}
-			prev = temp;
-			temp = temp.next;
-
-		}
-		System.out.println("** Not Found delete data **");
-	}
-
-	public void deleteStart() {
-		if (this.head == null) {
-			System.out.println("------Head is null------");
-			return ;
-		}
-		Node temp = this.head;
-		this.head = temp.next;
-		temp.next = null;
-	}
-
-	public void deleteEnd() {
-		if (this.head == null) {
-			System.out.println("------Head is null------");
-			return ;
-		}
-
-		Node temp = this.head;
-		if (temp.next == null) {
-			this.head = null;
-		}
-		while (temp.next.next != null) {
-			temp = temp.next;
-		}
-		temp.next = null;
-	}
-
-	public int length() {
+	public int getNodeLength() {
 		if (this.head == null) {
 			return 0;
 		}
@@ -169,38 +40,184 @@ public class LinkedList {
 		return count;
 	}
 	
-	public boolean isDataPresent(int data) {
+	public void insertAfter(int target, int data) {
+		Node newNode = new Node(data);
 		if (this.head == null) {
-			System.out.println(" head is null");
+			return;
+		}
+		Node temp = this.head;
+		while (temp != null) {
+			if (temp.data == target) {
+				newNode.next = temp.next;
+				temp.next = newNode;
+				return;
+			}
+			temp = temp.next;
+		}
+	}
+
+	public void insertAfterUsingPosition(int pos, int data) {
+		
+		if (this.head == null) {
+			if (pos == 1) {
+				Node newNode = new Node(data);
+				this.head = newNode;
+			}
+			return;
+		}
+		if (pos == 1) {
+			Node newNode = new Node(data);
+			newNode.next=this.head;
+			this.head=newNode;
+			return;
+		}
+		Node temp = this.head;
+		for (int count = 1;temp != null;count++,temp = temp.next) {
+			if (count == pos-1) {
+				Node newNode = new Node(data);
+				newNode.next = temp.next;
+				temp.next=newNode;
+				return;
+			}
+		}
+	}
+
+	public void insertEnd(int data) {
+		Node newNode = new Node(data);
+		if (this.head == null) {
+			this.head = newNode;
+			return;
+		}
+		Node temp = this.head;
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+
+		temp.next = newNode;
+	}
+
+	public void insertStart(int data) {
+		Node newNode = new Node(data);
+		newNode.next = this.head;
+		this.head = newNode;
+	}
+
+	public void deleteNodeWithGivenData(int target) {
+		if (this.head == null) {
+			return;
+		}
+		if (this.head.data == target) {
+			this.head = this.head.next;
+			return;
+		}
+		Node temp = this.head;
+		while (temp != null) {
+			if (temp.next.data == target) {
+				temp.next = temp.next.next;
+				return;
+			}
+			temp = temp.next;
+		}
+	}
+
+	public void deleteAfterUsingPosition(int pos) {
+		if (this.head == null) {
+			return;
+		}
+		
+		if (pos == 1) {
+			this.head = this.head.next;
+			return;
+		}
+		Node temp = this.head;
+		int count = 1;
+		while (temp != null) {
+			count++;
+			if (count == pos) {
+				temp.next = temp.next.next;
+				return;
+			}
+			temp = temp.next;
+		}
+	}
+
+	public void deleteStart() {
+		if (this.head == null) {
+			return;
+		}
+		this.head = this.head.next;
+	}
+
+	public void deleteEnd() {
+		if (this.head == null) {
+			return;
+		}
+
+		Node temp = this.head;
+		if (temp.next == null) {
+			this.head = null;
+		}
+		while (temp.next.next != null) {
+			temp = temp.next;
+		}
+		temp.next = null;
+	}
+
+	public boolean isPresent(int target) {
+		if (this.head == null) {
 			return false;
 		}
 		Node temp = this.head;
 		while (temp != null) {
-			if (temp.data == data) {
+			if (temp.data == target) {
 				return true;
 			}
 			temp = temp.next;
 		}
 		return false;
 	}
-	
-	public Node getDataNode (int data) {
+
+	public Node getDataNode(int target) {
 		if (this.head == null) {
-			System.out.println(" head is null");
 			return null;
 		}
 		Node temp = this.head;
-		if (temp.data == data){
+		if (temp.data == target) {
 			return this.head;
 		}
-		
+
 		while (temp.next != null) {
-			if (temp.next.data == data) {
+			if (temp.next.data == target) {
 				return temp.next;
 			}
 			temp = temp.next;
 		}
 		return null;
 	}
+	
+	public void reverse() {
+        Node prev = null;
+        Node current = this.head;
+        Node next = current.next;
+        while (current != null) {
+            next = prev;
+            prev = current;
+            current = next;
+        }
+        this.head = prev;
+    }
+	
+	public Node getFirstNode () {
+		if (head == null) {
+			return null;
+		}
+		return this.head;
+	}
+	
+	public int lengthRecursive(Node head) {
+		if (head == null) {
+			return 0;
+		}
+		return 1 + lengthRecursive(head.next);
+	}
 }
-
